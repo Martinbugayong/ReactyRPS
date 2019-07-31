256f8773ac9ed8f09a147807747fb1677a0e6e6d
martinbugayong <martinbugayong@gmail.com>
2019-07-31 11:46:31 -0700

    create a layout

---
+++ script.js
@@ -0,0 +1,17 @@
+const references = {
+    main: document.getElementById('mainSection'),
+    score: document.getElementById('scoreSection'),
+    button: document.getElementById('buttonSection'),
+    log: document.getElementById('logSection')
+}
+
+const choices = ['Rock', 'Paper', "Scissors"]
+
+/* ----- Set default state of application ----- */
+/* ----- Create buttons in Javascript for each item in the array ----- */
+/* ----- Create event handlers ----- */
+/* ----- Create your actions for player and computer ----- */
+/* ----- Create scoring function ----- */
+/* ----- Create round function ----- */
+/* ----- Create reset function ----- */
+/* ----- Create a render function that prints the changes on screen ----- */
