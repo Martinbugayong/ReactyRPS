0d09583806959ae6839dc99f097ce0acee7a298b
martinbugayong <martinbugayong@gmail.com>
2019-08-05 23:18:35 -0700

    Create buttons / create log text

--- script.js
+++ script.js
@@ -7,11 +7,42 @@ const references = {
 
 const choices = ['Rock', 'Paper', "Scissors"]
 
-/* ----- Set default state of application ----- */
 /* ----- Create buttons in Javascript for each item in the array ----- */
+choices.forEach(function (choice) {
+    const button = document.createElement('button');
+    button.innerHTML = choice;
+    button.id = `btn-${choice}` 
+    button.addEventListener('click', handleClick)
+    references.button.appendChild(button);
+})
+
 /* ----- Create event handlers ----- */
+function handleClick(e) {
+    // const log = e.target.innerHTML
+    const logP = document.createElement('p')
+    logP.innerHTML = e.target.innerHTML
+    references.log.innerHTML = '';
+    references.log.appendChild(logP);
+
+}
+
+// const playerChoice 
+function setPlayerChoice(){
+    handleClick()
+}
+
+
+/* ----- Set default state of application ----- */
+const defaultState = {
+    playerChoice: '',
+    cpuChoice: '',
+}
+
+/* ----- Set Player Choice ----- */
+
 /* ----- Create your actions for player and computer ----- */
 /* ----- Create scoring function ----- */
 /* ----- Create round function ----- */
 /* ----- Create reset function ----- */
 /* ----- Create a render function that prints the changes on screen ----- */
+/* -----  ----- */
